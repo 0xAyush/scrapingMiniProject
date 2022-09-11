1ef73432cbdbb5a1c333453e5c794d655cad239f
Ayush <xcplusplus9@gmail.com>
2022-09-11 11:41:00 +0530

    Added input functionality

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Importing axios from axios module and parse function from node-html-parser
 var axios = require("axios");
 var { parse } = require("node-html-parser");
+const { nextTick } = require("process");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // Scrape function to fetch the data from a webpage and show it's HTML Elements in the parsed data
-let scrape = async (url) => {
+let scrapeUrl = async (url) => {
   let data = await axios.get(url, {
     headers: {
       "User-Agent":
@@ -12,13 +19,24 @@ let scrape = async (url) => {
   });
 
   let res = parse(data.data)
-  console.log('Scraping price...')
   let price = res.querySelector('.a-price .a-offscreen').textContent
   console.log(price)
-  console.log('Done...')
+  console.log('Done...\n')
+  nextUrl()
   return price
 };
+
+function scrape(url) {
+  console.log(`Scraping price for ${url}...`)
+  scrapeUrl(url)
+}
 
 scrape(
   "https://amzn.eu/d/5SZAHrl"
 );
+
+function nextUrl() {
+  rl.question("Enter Amazon URL to scrape: ", function (answer) {
+    scrape(answer)
+  });
+}
